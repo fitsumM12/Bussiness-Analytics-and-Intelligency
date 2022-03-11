9161dacb3e72a55c4ae2dadd5062870d4e2e78ab
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-11 16:26:25 +0300

    update

--- WEEK 03/Q4_Create 1-D array, find out sum, max element, min element, mean.py
+++ WEEK 03/Q4_Create 1-D array, find out sum, max element, min element, mean.py
@@ -4,4 +4,37 @@ Created on Fri Mar 11 15:49:19 2022
 
 @author: fitsum
 """
+import numpy as np
+arr = np.array([1,2,3,4,55,6])
 
+#Find ou the sum of each element
+sum = 0 
+i = 0
+for i in range(0,len(arr)):
+    sum = sum + arr[i]
+print(f"\n The sum of each element: {sum}")
+
+#Max element
+MAX = arr[0]
+for i in range(0, len(arr)):
+    if MAX<arr[i]:
+        MAX = arr[i]
+    else:
+        continue
+print(f"\nThe maximum element is {MAX}")
+
+#MIN element
+MIN = arr[0]
+for i in range(0, len(arr)):
+    if MIN>arr[i]:
+        MIN = arr[i]
+    else:
+        continue
+print(f"\nThe minimum element is {MAX}")
+
+#MEAN of elemen
+res=0
+for i in range(0, len(arr)):
+    res = res+arr[i]
+mean = res/len(arr)
+print(f"\nThe mean of element is {mean}")
