e0a490c2c260f7e508a1a11bad65c3ad531a6b01
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-25 09:45:43 +0530

    update

--- WEEK 04/Q3_ Write a Pandas program to convert a NumPy array to a Pandas series.py
+++ WEEK 04/Q3_ Write a Pandas program to convert a NumPy array to a Pandas series.py
@@ -5,3 +5,12 @@ Created on Fri Mar 25 09:36:58 2022
 @author: fitsum
 """
 
+import numpy as np
+import pandas as pd
+np_array = np.array([10, 20, 30, 40, 50])
+print("NumPy array:")
+print(np_array)
+
+new_series = pd.Series(np_array)
+print("Converted Pandas Series:")
+print(new_series)
---
+++ WEEK 04/Q4_Write a Pandas program to convert the first column of a DataFrame as a Series.py
@@ -0,0 +1,18 @@
+# Q4_Write a Pandas program to convert the first column of a DataFrame as a Series
+"""
+Created on Fri Mar 25 09:41:41 2022
+
+@author: fitsum
+"""
+
+import pandas as pd
+d = {'col1':[1,2,3,4,7,11],
+     'col2':[0,9,8,7,4,33],
+     'col3':[1,3,4,5,7,22]}
+df = pd.DataFrame(data = d)
+print("Original DataFrame:")
+print(df)
+s1 = df.iloc[:,0]
+print("\n1st column as a Series:")
+print(s1)
+print(type(s1))
