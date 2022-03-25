b4b3c6076b2ff25c274e74cff92cf3044b4ec2f1
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-25 09:32:51 +0530

    update

---
+++ WEEK 04/Q1_Pandas program to create and display a one dimensional array like object containing an array of data.py
@@ -0,0 +1,11 @@
+# Q1.	Write a Pandas program to create and display a one-dimensional 
+# array-like object containing an array of data
+"""
+Created on Fri Mar 25 09:27:23 2022
+
+@author: fitsum
+"""
+
+import pandas as pd
+ds = pd.Series([2,4,6,8,9])
+print(ds)
---
+++ WEEK 04/Q2_Write a Pandas program to add, subtract, multiple and divide two Pandas Series.py
@@ -0,0 +1,7 @@
+# Q2_Write a Pandas program to add, subtract, multiple and divide two Pandas Series.
+"""
+Created on Fri Mar 25 09:31:45 2022
+
+@author: fitsum
+"""
+
